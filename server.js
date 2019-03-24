6a731255d701714a0d90834bb6b04e14429667c2
joann675 <joann675@yahoo.com>
2019-03-23 22:04:05 -0400

    fixed api to save note

--- server.js
+++ server.js
@@ -178,7 +178,8 @@ app.post("/articles/:id", function (req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, {  $push: { notes: dbNote._id }}, { new: true });
+     
     })
     .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
@@ -194,7 +195,7 @@ app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
-    .populate("note")
+    .populate("notes")
     .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
